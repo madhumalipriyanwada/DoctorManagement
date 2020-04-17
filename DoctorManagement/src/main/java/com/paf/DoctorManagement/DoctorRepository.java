9b32a56430131e828ea6530ad59b7b53c0e830ed
madhumalipriyanwada <madhumalipriyanwadha17@gmail.com>
2020-04-17 20:54:43 +0530

    DoctorRepository

--- DoctorManagement/src/main/java/com/paf/DoctorManagement/DoctorRepository.java
+++ DoctorManagement/src/main/java/com/paf/DoctorManagement/DoctorRepository.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.sql.Statement;
 import java.sql.Connection;
-//import java.sql.Date;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
